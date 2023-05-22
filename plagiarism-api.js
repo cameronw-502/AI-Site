944f5e3685cd9491464f887bf6f0adfbba2a73da
Cameron White <cameronwhite@Camerons-iMac.attlocal.net>
2023-05-21 22:37:35 -0400

    Adding plagiarism detector api

---
+++ plagiarism-api.js
@@ -0,0 +1,16 @@
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open('GET', 'https://plagiarism-detection-api.p.rapidapi.com/retrieve?job_id=1146335');
+xhr.setRequestHeader('X-RapidAPI-Key', '58c7dd1a2amshafb3783417139d8p17b413jsn7353d251c3d7');
+xhr.setRequestHeader('X-RapidAPI-Host', 'plagiarism-detection-api.p.rapidapi.com');
+
+xhr.send(data);
